1c35d11ff51df79d5744bef2c5db3dc16137b775
Asahel <asahelmain@ciencias.unam.mx>
2024-04-23 23:36:30 -0600

    Agrega CORS a flask

--- login-app-flask/app.py
+++ login-app-flask/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
-
+from flask_cors import CORS
 from db.alchemyClasses import db
 from controllers.auth.auth import auth_bp
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://aavaa:aavaa@localhost:3306/cienciasmart'
 app.config.from_mapping(
